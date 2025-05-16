394e883839d7df17a6672eba0b47e62c933e40c1
Peyman <peyman.osatian@gmail.com>
2025-05-16 02:17:51 +0200

    splash 2

---
+++ MyFurniture/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  MyFurniture
+//
+//  Created by Peyman Osatian on 2025-05-16.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
