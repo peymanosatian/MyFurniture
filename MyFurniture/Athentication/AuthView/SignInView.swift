874c26576ebfd6f55503bf03506239fc29932e95
Peyman <peyman.osatian@gmail.com>
2025-05-18 13:01:17 +0200

    onboardingSlider1

---
+++ MyFurniture/Athentication/AuthView/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  MyFurniture
+//
+//  Created by Peyman Osatian on 2025-05-18.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ MyFurniture/Onboarding/OnboardingSlide.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingSlide.swift
+//  MyFurniture
+//
+//  Created by Peyman Osatian on 2025-05-18.
+//
+
+import Foundation
